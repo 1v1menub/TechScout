786d04d509a12e23bed4a7554ba70df5f1d69bf1
estebanp@allmoxy.com <estebanp@allmoxy.com>
2022-11-18 13:42:59 -0500

    prettier setup cofig

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import SearchItem from './components/SearchItem/SearchItem'
-import "./App.css"
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchItem from './components/SearchItem/SearchItem';
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<SearchItem img="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OHx8fGVufDB8fHx8&w=1000&q=80" name="Laptop gamer prosaza" rating={4.6} tags={["gamer", "caro", "nvidia", "hdmi"]} price={5000} shopname="tuvi" description="lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff"/>} />
-    </Routes>
-  </BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SearchItem
+                img="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OHx8fGVufDB8fHx8&w=1000&q=80"
+                name="Laptop gamer prosaza"
+                rating={4.6}
+                tags={['gamer', 'caro', 'nvidia', 'hdmi']}
+                price={5000}
+                shopname="tuvi"
+                description="lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff"
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
