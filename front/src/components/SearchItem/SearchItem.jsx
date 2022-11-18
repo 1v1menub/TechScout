bb1fd9fdbb7365462991deab878ec8a9bb5f57f6
1v1menub <ignacio.rubio@utec.edu.pe>
2022-11-17 23:41:00 -0500

    SearchItem component

---
+++ front/src/components/SearchItem/SearchItem.jsx
@@ -0,0 +1,30 @@
+import "./SearchItem.css"
+import ratingstar from "../../assets/ratingstar.svg"
+
+function SearchItem({img, name, price, shopname, description, rating, tags}) {
+
+    return (
+        <div className="search-item-cont">
+            <img className="search-item-img" src={img}></img>
+            <div className="info-cont">
+                <div className="title-rating-cont">
+                    <span className="search-item-title">{name}</span>
+                    <span className="search-item-rating">{rating}</span>
+                    <img className="rating-star" src={ratingstar}></img>
+                    <span>Precio: S/ {price}</span>
+                </div>
+                <div className="description-cont">
+                    <span className="search-item-description">{description}</span>
+                </div>
+                <div className="tags-cont">
+                    {tags.map((tagname) => {
+                        return <div className="tag">{tagname}</div>
+                    })}
+                <a className="shop" href={shopname}>{shopname}</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchItem
