c943e0110a325d868958d88de2ccf5350d22ae7c
1v1menub <ignacio.rubio@utec.edu.pe>
2022-11-17 21:25:21 -0500

    react base

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      lolon
+    </div>
+  )
+}
+
+export default App
