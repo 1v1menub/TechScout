7a70e12a89d078ee826652b04f6b41450023ba81
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-11-25 13:30:51 -0500

    ui fixes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -21,21 +21,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <SearchItem
-                img="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OHx8fGVufDB8fHx8&w=1000&q=80"
-                name="Laptop gamer prosaza"
-                rating={4.6}
-                tags={['gamer', 'caro', 'nvidia', 'hdmi']}
-                price={5000}
-                shopname="tuvi"
-                description="lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff lorem ipsum jaja diff"
-              />
-            }
-          />
-
           {/* enviar busqueda aca */}
           <Route
             path="/search"
@@ -128,7 +113,7 @@ function App() {
             }
           />
           <Route
-            path="/welcomescreen"
+            path="/"
             element={
               <div className="testcont3">
                 <SideBar
--- front/src/components/ProductCard/ProductCardSearch.jsx
+++ front/src/components/ProductCard/ProductCardSearch.jsx
@@ -7,23 +7,29 @@ function ProductCardSearch({ image, name, rating, price, tags }) {
     <div className="product-card-search-cont">
       <div className="product-card-search-top-cont" style={{ backgroundImage: `url(${image})` }}>
         <div className="product-card-search-rating-cont">
-          <span className="product-card-search-rating">{rating}</span>
+          <div className="inner-rating">
+          <svg viewBox="0 16 12 12">
+            <text x="1.5" y="25">
+              {rating}
+            </text>
+          </svg>
           <img className="product-card-search-timelogo" src={ratingstar}></img>
+
+          </div>
         </div>
       </div>
       <div className="product-card-search-bottom-cont">
         <span className="product-card-search-name">{name}</span>
         <div className="product-card-search-info-cont">
           <svg viewBox="0 15 80 12" className="product-card-search-dtime-sample-text">
-            <text x="10" y="15">Precios desde:</text>
+            <text x="10" y="15">
+              Precios desde:
+            </text>
           </svg>
           <svg viewBox="0 0 56 18" className="product-card-search-dtime">
             <text x="0" y="15">
               S/.{price}
             </text>
-            {/* <text x="0" y="15">
-              S/.{price}
-            </text> */}
           </svg>
         </div>
       </div>
